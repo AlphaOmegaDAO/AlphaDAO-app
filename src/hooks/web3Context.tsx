de82cdc85938eb6213b796310de2928e0f0925fe
Hodlsage <hodlsage@protonmail.com>
2022-01-10 17:27:04 -0500

    for Seth

--- src/hooks/web3Context.tsx
+++ src/hooks/web3Context.tsx
@@ -69,10 +69,10 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({ chil
   const [connected, setConnected] = useState(false);
   // NOTE (appleseed): if you are testing on rinkeby you need to set chainId === 4 as the default for non-connected wallet testing...
   // ... you also need to set getTestnetURI() as the default uri state below
-  const [chainID, setChainID] = useState(97);
+  const [chainID, setChainID] = useState(56);
   const [address, setAddress] = useState("");
 
-  const [uri, setUri] = useState(getTestnetURI());
+  const [uri, setUri] = useState(getMainnetURI());
 
   const [provider, setProvider] = useState<JsonRpcProvider>(new StaticJsonRpcProvider(uri));
 
@@ -86,7 +86,7 @@ export const Web3ContextProvider: React.FC<{ children: ReactElement }> = ({ chil
           options: {
             rpc: {
               56: getMainnetURI(),
-              97: getTestnetURI(),
+              4: getTestnetURI(),
             },
           },
         },
